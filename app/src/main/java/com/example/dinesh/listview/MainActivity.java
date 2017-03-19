c1570494f0121ffcbabded4ba9ed92d0e31ccd2c
Dinesh Chabra <dinesh_chabra@yahoo.com>
2017-03-19 20:30:05 +0530

    Listview - By Defining Listview

--- app/src/main/java/com/example/dinesh/listview/MainActivity.java
+++ app/src/main/java/com/example/dinesh/listview/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.dinesh.listview;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ListActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        String[] values = new String[] { "DINESH", "KOVID", "NEERU",
+                "Manmohan", "Dubey", "Sushil", "Javed" };
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, values);
+        setListAdapter(adapter);
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        String item = (String) getListAdapter().getItem(position);
+        Toast.makeText(this, item + " selected", Toast.LENGTH_LONG).show();
     }
 }
